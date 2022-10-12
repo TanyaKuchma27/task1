805808e033a1fb90ecb81c17d001f3af374f3541
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-10-12 14:20:50 +0400

    update imports

--- src/comps/Table/Table.jsx
+++ src/comps/Table/Table.jsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
 import { useEffect, useState } from "react";
 import axios from 'axios';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper }  from '@mui/material';
 
 const BasicTable = () => { 
   const [users, setUsers] = useState([]);
